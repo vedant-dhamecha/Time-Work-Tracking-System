7544d8d4c6e8604081aacfc1f1b08e78c21657ae
KushangJari3092 <kushangjari3092@gmail.com>
2023-08-18 15:25:27 +0530

    login pages commitingg

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,6 +17,7 @@ export default function Login() {
     const [password, setPassword] = useState('')
     // const [msg, setMsg] = useState('')
 
+    const handleLogin = () => { }
     // const handleLogin = async (e) => {
     //     e.preventDefault();
 
