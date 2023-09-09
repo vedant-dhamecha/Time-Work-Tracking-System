9086cd10343160c5b48b815daadbb36e7c7a4636
KushangJari3092 <kushangjari3092@gmail.com>
2023-09-09 14:39:55 +0530

    .a.a

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import '../styles/projects.css'
 
 export default function Projects({ projectName }) {
+
+    const getProjects = async () => {
+
+        try {
+            const res = await fetch(`http://localhost:3218/getProject`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: "include",
+                body: JSON.stringify({ time })
+            })
+
+            const data = await res.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <>
             <div className='proj'>
